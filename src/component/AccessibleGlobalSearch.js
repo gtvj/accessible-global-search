6003bc42d1ce5abf99f6ba2637e0384dd0dd9d8e
Gwyn Jones <m@gwnjns.com>
2018-10-27 18:29:36 +0100

    Add search URLs

--- src/component/AccessibleGlobalSearch.js
+++ src/component/AccessibleGlobalSearch.js
@@ -1,24 +1,26 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './AccessibleGlobalSearch.css';
 
 class AccessibleGlobalSearch extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      select_type: 'Select a search type'
+    constructor(props) {
+        super(props);
+        this.state = {
+            select_type: 'Select a search type',
+            website_search_url: 'http://www.nationalarchives.gov.uk/search/results',
+            discovery_search_url: 'http://discovery.nationalarchives.gov.uk/results/'
+        }
     }
-  }
 
-  render() {
-    return (
-      <form className="global-search-js">
-        <fieldset>
-          <legend>{this.state.select_type}</legend>
-        </fieldset>
-      </form>
-    );
-  }
+    render() {
+        return (
+            <form className="global-search-js">
+                <fieldset>
+                    <legend>{this.state.select_type}</legend>
+                </fieldset>
+            </form>
+        );
+    }
 }
 
 export default AccessibleGlobalSearch;
--- src/component/AccessibleGlobalSearch.test.js
+++ src/component/AccessibleGlobalSearch.test.js
@@ -15,6 +15,8 @@ it('renders without crashing', () => {
 
 it('has the correct props', () => {
     expect(instance.state.select_type).toBe('Select a search type');
+    expect(instance.state.website_search_url).toBe('http://www.nationalarchives.gov.uk/search/results');
+    expect(instance.state.discovery_search_url).toBe('http://discovery.nationalarchives.gov.uk/results/');
 });
 
 it('renders a form with the correct className', () => {
@@ -30,9 +32,7 @@ it('contains a fieldset and legend with the correct text', () => {
     const legend = test_instance.findByType('legend');
     expect(legend.type).toBe('legend');
     expect(legend.children[0]).toBe(instance.state.select_type);
-
 });
-
 
 
 
