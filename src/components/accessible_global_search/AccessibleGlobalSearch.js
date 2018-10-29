cca205b8a6d181e4ab16d08a26dca1b1c9914ea9
Gwyn Jones <m@gwnjns.com>
2018-10-29 20:05:06 +0000

    Basic implementation: switch search destination
    This will be refactored as there's quite a bit of redundancy in the
    code. I intend to change the structure of state to make it simpler
    and more dynamic.

--- src/components/accessible_global_search/AccessibleGlobalSearch.js
+++ src/components/accessible_global_search/AccessibleGlobalSearch.js
@@ -7,12 +7,18 @@ class AccessibleGlobalSearch extends Component {
 
     constructor(props) {
         super(props);
+        this.handle_search_selection = this.handle_search_selection.bind(this);
         this.state = {
-            select_type: 'Select a search type',
+
+            active_search_url: 'http://www.nationalarchives.gov.uk/search/results',
+            active_search_label: 'Search the website',
+
+            search_query: 'Enter search term',
             website_search_url: 'http://www.nationalarchives.gov.uk/search/results',
             discovery_search_url: 'http://discovery.nationalarchives.gov.uk/results/',
             search_options: {
                 group_name: 'search_type',
+                select_type: 'Select a search type',
                 options: [
                     {
                         label: 'Search Discovery',
@@ -27,12 +33,36 @@ class AccessibleGlobalSearch extends Component {
         }
     }
 
+    handle_search_selection(e) {
+        console.log(`The element was ${e.target.id}`);
+
+        if (e.target.id === 'discovery_search') {
+            this.setState({
+                active_search_url: this.state.discovery_search_url,
+                active_search_label: 'Search Discovery'
+                });
+        }
+
+        if (e.target.id === 'website_search') {
+            this.setState({
+                active_search_url: this.state.website_search_url,
+                active_search_label: 'Search the website'
+            });
+        }
+    }
+
     render() {
         return (
-            <form className="global-search-js">
-                <fieldset>
-                    <legend>{this.state.select_type}</legend>
-                    <SearchOption group_name={this.state.search_options.group_name} options={this.state.search_options.options}/>
+            <form role="search" className="global-search-js" action={this.state.active_search_url}
+                  onChange={this.handle_search_selection}>
+                <fieldset id="select-search-type">
+                    <legend>{this.state.search_options.select_type}</legend>
+                    <SearchOption group_name={this.state.search_options.group_name}
+                                  options={this.state.search_options.options}/>
+                </fieldset>
+                <fieldset id="search-query">
+                    <legend>{this.state.search_query}</legend>
+                    <input type="search" role="search" aria-label={this.state.active_search_label}/>
                 </fieldset>
             </form>
         );
--- src/components/accessible_global_search/AccessibleGlobalSearch.test.js
+++ src/components/accessible_global_search/AccessibleGlobalSearch.test.js
@@ -14,42 +14,67 @@ it('renders without crashing', () => {
 });
 
 it('has the correct props', () => {
-    expect(instance.state.select_type).toBe('Select a search type');
+    expect(instance.state.search_options.select_type).toBe('Select a search type');
     expect(instance.state.website_search_url).toBe('http://www.nationalarchives.gov.uk/search/results');
     expect(instance.state.discovery_search_url).toBe('http://discovery.nationalarchives.gov.uk/results/');
 });
 
-it('renders a form with the correct className', () => {
+it('renders a form with the correct className and action', () => {
     const rendered_component = test_renderer.toJSON();
     expect(rendered_component.type).toBe('form');
     expect(rendered_component.props.className).toBe('global-search-js');
+    expect(rendered_component.props.role).toBe('search');
+    expect(rendered_component.props.action).toBe('http://www.nationalarchives.gov.uk/search/results');
 });
 
-it('contains a fieldset and legend with the correct text', () => {
-    const fieldset = test_instance.findByType('fieldset');
+it(`contains a 'select-search-type' fieldset and legend with the correct text`, () => {
+    const fieldset = test_instance.findByProps({id: 'select-search-type'});
     expect(fieldset.type).toBe('fieldset');
 
-    const legend = test_instance.findByType('legend');
+    const legend = fieldset.findByType('legend');
     expect(legend.type).toBe('legend');
-    expect(legend.children[0]).toBe(instance.state.select_type);
+    expect(legend.children[0]).toBe(instance.state.search_options.select_type);
 });
 
 it('contains a radio button and label for website search', () => {
-    const website_search_radio = test_instance.findByProps({ id: 'website_search'});
+    const website_search_radio = test_instance.findByProps({id: 'website_search'});
     expect(website_search_radio.type).toBe('input');
 
-    const website_search_label = test_instance.findByProps({ htmlFor: 'website_search'});
+    const website_search_label = test_instance.findByProps({htmlFor: 'website_search'});
     expect(website_search_label.type).toBe('label');
 });
 
 it('contains a radio button and label for discovery search', () => {
-    const discovery_search_radio = test_instance.findByProps({ id: 'discovery_search'});
+    const discovery_search_radio = test_instance.findByProps({id: 'discovery_search'});
     expect(discovery_search_radio.type).toBe('input');
 
-    const discovery_search_label = test_instance.findByProps({ htmlFor: 'discovery_search'});
+    const discovery_search_label = test_instance.findByProps({htmlFor: 'discovery_search'});
     expect(discovery_search_label.type).toBe('label');
 });
 
+it('changes the form action to Discovery when that option is selected', () => {
 
+    // Test that the component is updated in response to a change event
+    // Have posted a question on Stack Overflow:
+    // https://stackoverflow.com/questions/53026675/
+
+});
+
+it(`contains a 'search-query' fieldset and legend with the correct text`, () => {
+    const fieldset = test_instance.findByProps({id: 'search-query'});
+    expect(fieldset.type).toBe('fieldset');
+
+    const legend = fieldset.findByType('legend');
+    expect(legend.type).toBe('legend');
+    expect(legend.children[0]).toBe(instance.state.search_query);
+});
+
+it(`contains a labelled search element with the correct properties`, () => {
+    const fieldset = test_instance.findByProps({id: 'search-query'});
+    const search_field = fieldset.findByProps({role: 'search'});
+    expect(search_field.props.type).toBe('search'); // This tests the 'type' attribute
+    expect(search_field.type).toBe('input');
+    expect(search_field.props['aria-label']).toBe(instance.state.active_search_label);
+});
 
 
